f33c0e467115cc5999e0d236cdcc121bb2e50570
Laércio Oliveira <jose.laercio.bezerra@gmail.com>
2020-11-11 01:19:53 -0300

    feat: install and config bcryptjs

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const bcryptjs = require("bcryptjs");
 
 const UserSchema = new Schema({
   name: {
@@ -20,6 +21,12 @@ const UserSchema = new Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+UserSchema.pre("save", async function (next) {
+  const hash = await bcryptjs.hash(this.password, 10);
+  this.password = hash;
+  next();
 });
 
 module.exports = model("User", UserSchema);
