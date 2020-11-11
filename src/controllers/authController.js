903bd8173a605551bb0158e63a868bb37863e2e0
Laércio Oliveira <jose.laercio.bezerra@gmail.com>
2020-11-11 11:28:30 -0300

    feat: create authenticate method

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 module.exports = {
   async createUser(req, res) {
@@ -9,10 +10,29 @@ module.exports = {
         return res.status(400).send({ error: "User already exists" });
 
       const user = await User.create(req.body);
+      user.password = undefined;
 
       return res.send({ user });
     } catch (err) {
       return res.status(400).send({ error: "Registration failed" });
     }
+  },
+
+  async authenticate(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status(400).send({ error: "User not found!" });
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      return res.status(400).send({ error: "Invalid password!" });
+    }
+
+    user.password = undefined;
+
+    return res.send({ user });
   },
 };
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ const authController = require("./controllers/authController.js");
 const router = Router();
 
 router.post("/create-user", authController.createUser);
+router.post("/authenticate", authController.authenticate);
 
 module.exports = router;
